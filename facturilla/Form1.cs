c62709292a0e0f01da2877a21679162f4622b1c5
Juanarielok <82241132+Juanarielok@users.noreply.github.com>
2022-02-22 22:38:05 -0300

    Add files via upload

---
+++ facturilla/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace facturilla
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void PRECIO_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a, b,res;
+            a=int.Parse(txt1.Text);
+            b=int.Parse(txt2.Text);
+            res = a + b;
+            txtRes.Text= res.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+    }
+}
---
+++ facturilla/cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace facturilla
+{
+    internal class cliente
+    {
+       public DataGridViewRowCollection compras;
+        public string nombre;
+        public cliente (string nombre, DataGridViewRowCollection compras)
+        {
+            this.nombre = nombre;
+            this.compras = compras;
+
+        }
+    }
+}
